ba282375dd41eda50448cc131aadce834a2cc4a9
AlexTrubkina <sashatru99@gmail.com>
2024-05-30 21:50:28 +0300

    feat: add min-width and box-shadow

--- src/components/Card/ui/CardBody.jsx
+++ src/components/Card/ui/CardBody.jsx
@@ -2,7 +2,7 @@ export function CardBody ({ title, description }) {
     return (
         <div className="card__body">
             <h3 className="card__title">{title}</h3>
-            <p className="card__description">{description}</p>
+            <p className="card__description">{`${description.substring(0, 200)}...`}</p>
         </div> 
     )
 }
